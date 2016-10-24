562e8d80172a921b95335bc61330ffa904acf233
bahamutr13 <tedlpascua@gmail.com>
2016-10-23 22:22:00 -0700

    ok im going to finish this for reals

--- app/src/main/java/com/app/controller/MainController.java
+++ app/src/main/java/com/app/controller/MainController.java
@@ -16,7 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMapping;;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
@@ -44,20 +44,27 @@ public class MainController {
     @RequestMapping(value = "/employee/name")
     public String listHotels(Model model){
         JdbcTemplate jdbcTemplate = MainController.getJdbcTemplate();
-        //int rowCount = jdbcTemplate.queryForObject("select count(*) from BAJS_EMPLOYEE", Integer.class);
+        //int rowCount = jdbcTemplate.queryForObject("select count(*) from employeeHarvest", Integer.class);
+        System.out.println("DID I EVER HAPPEN");
+
 
         List<String> stringList = jdbcTemplate.query(
-            "select * from BAJS_EMPLOYEE",
+            "select * FROM har_employee",
             new RowMapper<String>() {
                 public String mapRow(ResultSet rs, int rowNum) throws SQLException {
-                    String str = new String(rs.getString("first_name"));
-                    return str;
+                    String str = new String(rs.getString("NAMEFIRST"));
+                    return seeAndPut(str);
                 }
         });
 
         model.addAttribute("name", stringList);
+        System.out.println(stringList);
         return "jsonTemplate";
-
+    }
+    
+    public String seeAndPut(String str){
+      System.out.println("test" + str);
+      return str;
     }
 
 
--- app/src/main/webapp/WEB-INF/js/controllers/main.js
+++ app/src/main/webapp/WEB-INF/js/controllers/main.js
@@ -11,6 +11,7 @@ angular.module('WebApp')
 .controller('MainCtrl', function ($scope, $log, $http) {
 
   $scope.getEmployeeNames = function() {
+    console.log("did it happen?");
     $http({
       url: '/app/employee/name',
       method: "GET"
@@ -20,10 +21,14 @@ angular.module('WebApp')
       $scope.emp = data;
 
       $log.debug(data);
+      console.log($scope.emp);
 
     }).error(function (data, status, headers, config) {
       $log.error(status + " Error obtaining name data: " + data);
     });
+  }
+  $scope.doSomethingNOW = function(){
+    getEmployeeNames();
   }
 
 });
